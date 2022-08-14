3fc25de7aa41ff183fcaa7007234d098be5cac00
Ramon Ives <ramonivesdev@gmail.com>
2022-08-14 17:47:42 -0300

    completando desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,105 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Header } from "./components/Header";
-import { PlusCircle } from "phosphor-react"
+import { PlusCircle } from "phosphor-react";
 import { EmptyList } from "./components/EmptyList";
-
-import styles from "./App.module.css"
-import "./global.css";
 import { Task } from "./components/Task";
 
+import styles from "./App.module.css";
+import "./global.css";
+
 export function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: Math.random(),
+      content: "Primeira task",
+      done: false,
+    },
+    {
+      id: Math.random(),
+      content: "Segunda task",
+      done: true,
+    },
+  ]);
+  const [task, setTask] = useState("");
 
-  const arr = [1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4,1, 2, 3, 4]
+  function handleCreateNewTask(e: FormEvent) {
+    e.preventDefault();
 
+    const newTask = {
+      id: Math.random(),
+      content: task,
+      done: false,
+    };
 
-  const existList = arr.length === 0
+    setTasks([...tasks, newTask]);
+    setTask("");
+  }
+
+  function deleteTask(id: number) {
+    const tasksWithoutDeletedOne = tasks.filter((task) => task.id !== id);
+
+    setTasks(tasksWithoutDeletedOne);
+  }
+
+  function handleNewTask(e: ChangeEvent<HTMLInputElement>) {
+    setTask(e.target.value);
+  }
+
+  function toggleTaskDone(id: number) {
+    const updatedTask = tasks.map((task) =>
+      task.id === id ? { ...task, done: !task.done } : task
+    );
+
+    setTasks(updatedTask);
+  }
+
+  const existList = tasks.length === 0;
+  const isNewTask = task.length === 0;
 
   return (
     <div>
       <Header />
       <div className={styles.container}>
-        <div className={styles.containerRow}>
+        <form onSubmit={handleCreateNewTask} className={styles.containerRow}>
           <input
             className={styles.input}
             type="text"
             placeholder="Adicione uma nova tarefa"
+            value={task}
+            onChange={handleNewTask}
+            required
           />
-          <button
-            className={styles.button}
-            type="submit"
-          >
+          <button className={styles.button} type="submit" disabled={isNewTask}>
             Criar
             <PlusCircle size={24} />
           </button>
-        </div>
+        </form>
 
         <div className={styles.countTasks}>
           <div className={styles.containerRow}>
-            <p className={styles.createdTasks}>Tarefas criadas
+            <p className={styles.createdTasks}>
+              Tarefas criadas
               <p className={styles.amount}>0</p>
             </p>
-            <p className={styles.completedTasks}>Concluidas
+            <p className={styles.completedTasks}>
+              Concluidas
               <p className={styles.amount}>0</p>
             </p>
           </div>
         </div>
 
-        {
-          existList ? (
-            <EmptyList />
-          ) : (
-            arr.map(task => <Task />)
-          )
-        }
-
+        {existList ? (
+          <EmptyList />
+        ) : (
+          tasks.map((task) => (
+            <Task
+              task={task}
+              onDeleteTask={deleteTask}
+              onToggleTaskDone={toggleTaskDone}
+            />
+          ))
+        )}
       </div>
     </div>
-  )
+  );
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,12 +1,36 @@
-import { Trash } from "phosphor-react"
-import styles from "./Task.module.css"
+import { Trash } from "phosphor-react";
+import styles from "./Task.module.css";
 
-export function Task() {
-    return (
-        <div className={styles.container}>
-            <input type="checkbox" />
-            <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-            <button><Trash size={24} /></button>
-        </div>
-    )
+interface ITask {
+  id: number;
+  content: string;
+  done: boolean;
+}
+
+interface ITaskProps {
+  task: ITask;
+  onDeleteTask: (id: number) => void;
+  onToggleTaskDone: (id: number) => void;
+}
+
+export function Task({ task, onDeleteTask, onToggleTaskDone }: ITaskProps) {
+  function handleDeleteTask() {
+    onDeleteTask(task.id);
+  }
+
+  function handleToggleTaskDone(){
+    onToggleTaskDone(task.id)
+  }
+
+  return (
+    <div className={task.done ? styles.containerDone : styles.container}>
+      <input className={styles.input} type="checkbox" checked={task.done} onChange={handleToggleTaskDone} />
+      <p className={task.done ? styles.textDone : styles.text}>
+        {task.content}
+      </p>
+      <button className={styles.button} onClick={handleDeleteTask}>
+        <Trash size={24} />
+      </button>
+    </div>
+  );
 }
